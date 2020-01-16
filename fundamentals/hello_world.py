015b9532b3b61d04f904bb7068b052d2d64004e8
Manny <mannyinwang@gmail.com>
2020-01-16 15:19:41 -0800

    initial commit

---
+++ fundamentals/hello_world.py
@@ -0,0 +1,23 @@
+print("Hello World")
+first_name = "Manny"
+print("Hello", first_name)
+print("Hello " + first_name)
+age = 32
+print("Hello",age)
+# This gives an error
+# print("Hello "+ age) 
+print("Hello", + age)
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}".format(fave_food1, fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}")
+
+
+
+
+
+
+
+
+
+
